b5c268fb2ada301eb9bb3b452ce5b3b525ab486a
Maya Bloom <Maya.Bloom@ou.edu>
2021-02-02 18:10:53 -0600

    In class example on how to add doubles and integers

---
+++ In Class Example Feb 1/Variables Input Conversion Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variables_Input_Conversion_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Please enter a double >>");
+        }   string answer = Console.ReadLine();
+            heresMyDouble = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter an integer >>");
+            answer = Console.ReadLine();
+            heresMyInt = Convert.ToInt32(answer);
+
+
+
+            double result = heresMyDouble + heresMyInt;
+
+        Console. WriteLine("The result of " + heresMyDouble.ToString("N3") + " + " +
+            heresMyInt.ToString() + " = " + result.ToString("C5"));
+
+
+    }
+}
---
+++ In Class Example Feb 1/Variables Inputs Conversions Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variables_Inputs_Conversions_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            int heresMyInt;
+            double heresMyDouble;
+     
+            Console.WriteLine($"Please enter a double >>");
+            string answer = Console.ReadLine();
+            heresMyDouble = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter an integer >>");
+            answer = Console.ReadLine();
+            heresMyInt = Convert.ToInt32(answer);
+
+            double result = heresMyDouble + heresMyInt;
+
+            Console.WriteLine("The result of " + heresMyDouble.ToString("N3") + "+ "
+                heresMyInt.ToString() + " = " + result.ToString("C5"));
+        }
+    }
+}
