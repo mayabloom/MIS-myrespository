f7cc717c411040768180fd24ffae1134c3c7af29
Maya Bloom <Maya.Bloom@ou.edu>
2021-02-10 13:01:38 -0600

    Fizz Buzz Example

---
+++ In Class Fizz Buzz/In Class Fizz Buzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace In_Class_Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumberBetween1and100 = rand.Next(1, 101);
+
+            if (randomNumberBetween1and100 % 3 ==0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            if (randomNumberBetween1and100 % 5 ==0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            if (randomNumberBetween1and100 % 3 == 0
+                && randomNumberBetween1and100 % 5 ==0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+
+            else
+            {
+                Console.WriteLine($"{randomNumberBetween1and100}");
+            }
+        }
+    }
+}
