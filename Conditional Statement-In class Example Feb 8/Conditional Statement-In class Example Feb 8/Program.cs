b2e0c6d959dd07159f8fee3dd0170d88c5561e0d
Maya Bloom <Maya.Bloom@ou.edu>
2021-02-10 11:15:40 -0600

    In class example completed

---
+++ Conditional Statement-In class Example Feb 8/Conditional Statement-In class Example Feb 8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Conditional_Statement_In_class_Example_Feb_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pease enter your favorite #  whole #'s only!)>>");
+            //int luckyNumber = Convert.ToInt32((Console.ReadLine()); //First way
+            string answer = Console.ReadLine(); //second way part a 
+            int luckyNumber;
+            bool isInteger = int.TryParse(answer, out luckyNumber);
+
+            if (isInteger== false)
+            {
+                Console.WriteLine($"{answer} was not a valid integer Goodbye!");
+                Environment.Exit(-1);
+                //return; Another way to exit the application
+
+
+            }
+
+            luckyNumber = Convert.ToInt32(answer);// second way part b
+            int remainder = luckyNumber % 7;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine($"{luckyNumber} is truly a lucky number!");
+
+            }
+            else if (luckyNumber % 13 == 0)
+            {
+                Console.WriteLine($"{luckyNumber} is really an unlucky number!");
+            }
+
+            else
+            {
+                Console.WriteLine($"{luckyNumber}");
+            }
+
+            Console.WriteLine("What is your favorite type of pet >>");
+            answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+
+                case "dog":
+                    Console.WriteLine("Cool me too!");
+                    break;
+                case "cat":
+                    Console.WriteLine("If you say so");
+                    break;
+                default:
+                    Console.WriteLine($"Who would ever have a pet {answer}!??");
+                    break;
+            }
+        }
+    }
+}
