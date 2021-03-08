b1d8fde5347a1038bb57c2b9ecce41fb9cdf9a59
Maya Bloom <Maya.Bloom@ou.edu>
2021-03-08 12:17:44 -0600

    re did hw 1

---
+++ Re do hW/Re do hW/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Re_do_hW
+{
+    class Program
+    {
+        const double SALES_TAX = .085;
+        static void Main(string[] args)
+        {
+            string productname;
+            double tickets;
+            double ticketprice;
+
+            Console.WriteLine("What is the name of the item you are purchasing? ");
+            string response = Console.ReadLine();
+            productname = Convert.ToString(response);
+
+            Console.WriteLine($"How many {productname} ticket's do you want to buy?");
+            string answer = Console.ReadLine(); 
+            tickets = Convert.ToDouble(answer);
+
+            Console.WriteLine($"What is the price for each {productname} Ticket?");
+            string price = Console.ReadLine();
+            ticketprice = Convert.ToDouble(price);
+
+            double subtotal = tickets * ticketprice;
+            double salestax = subtotal * SALES_TAX;
+            double total = subtotal + salestax;
+
+
+            Console.WriteLine($"Your subtotal for your bill is {subtotal.ToString("C2")} ");
+            Console.WriteLine($"Your sales tax for your bill is {salestax.ToString("C2")} ");
+            Console.WriteLine($"Your total for your bill is {total.ToString("C2")} ");
+
+
+
+        }
+    }
+}
--- String Manipulation/String Manipulation/Program.cs
+++ String Manipulation/String Manipulation/Program.cs
@@ -4,27 +4,44 @@ namespace String_Manipulation
 {
     class Program
     {
+        public static int Indexofspace { get; private set; }
+
         static void Main(string[] args)
         {
             string message = "tAylOr SwIFT, tHe WeEknD, TRaVis sCoTt, aRiANa gRAnE, bILLiE eiLiSH";
+            ///            01234567
+            string msg2 = "tAylOr swIFT";
 
-            string newMessage = message.ToUpper()[O] + message.ToLower().Substring(1);
+            for (int i - 0; i < msg2.length; i++)
+
+            {
+                Console.WriteLine(msg2(i));
+            }
+
+            int indexofSpace = msg2.IndexOf(' ');
+            msg2 = msg2.ToUpper()[0] + msg2.Substring(1, Indexofspace);
+            +msg2.ToUpper()[indexofSpace+1] + msg2.ToLower().Substring(indexofSpace + 2);
+
+            Console.WriteLine(msg2);
+            return; 
+
+            string newMessage = message.ToUpper()[0] + message.ToLower().Substring(1);
 
             string[ ] artists = message.Split(',');
 
             for (int i = 0; 1 < artists.Length; i++)
             {
-                string artist = artists[i].Trim;
+                string artist = artists[i].Trim();
 
-                string[] names = artist.Split(' ');
+                string[] names = artists.Split(' ');
                 string firstName = names[0];
                 string lastName = names[1];
 
 
-                string newArtist = firstName.ToUpper()[O] + firstName.ToLower().Substring(1);
-                                  lastName.ToUpper()[O] + lastName.ToLower().Substring(1);
+                string newArtist = firstName.ToUpper()[0] + firstName.ToLower().Substring(1);
+                                  lastName.ToUpper()[0] + lastName.ToLower().Substring(1);
 
-                Console.WriteLine(artiss[i]);
+                Console.WriteLine(artists[i]);
             }
 
             Console.WriteLine(newMessage);
