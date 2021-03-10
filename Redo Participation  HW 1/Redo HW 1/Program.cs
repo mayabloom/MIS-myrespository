148ae3f3129a38b2b77d4cc164f8b4850a8693be
Maya Bloom <Maya.Bloom@ou.edu>
2021-03-09 19:12:15 -0600

    redid hw 1 student grades

---
+++ Redo Participation  HW 1/Redo HW 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Redo_HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double SALES_TAX = .085;
+
+            Console.WriteLine("What is the name of the item you are purchasing?");
+            string productname = Console.ReadLine();
+
+            double Tickets;
+
+            Console.WriteLine($"How many {productname} ticket's do you want to buy?");
+            string ticketnumber = Console.ReadLine();
+            Tickets = Convert.ToDouble(ticketnumber);
+
+            double ticketprice;
+
+            Console.WriteLine($"What is the price for each {productname} Ticket?");
+            string price = Console.ReadLine();
+            ticketprice = Convert.ToDouble(price);
+
+            double subtotal = Tickets * ticketprice;
+            double salestax = subtotal * SALES_TAX;
+            double total = subtotal + salestax;
+
+            Console.WriteLine($"Your subtotal for your bill is {subtotal.ToString("C2")}");
+            Console.WriteLine($"Your sales tax for your bill is {salestax.ToString("C2")}");
+            Console.WriteLine($"Your total for your bill is {total.ToString("C2")}");
+
+
+
+        }
+    }
+}
---
+++ Redo Participation  HW 1/Redo student grade/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Redo_student_grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastname = Console.ReadLine();
+
+            double studentid;
+
+            Console.WriteLine("What is your student id?");
+            string id = Console.ReadLine();
+            studentid = Convert.ToDouble(id);
+
+            double homeworkgrade;
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            string hwgrade = Console.ReadLine();
+            homeworkgrade = Convert.ToDouble(hwgrade);
+
+            double participationgrade;
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            string participation = Console.ReadLine();
+            participationgrade = Convert.ToDouble(participation);
+
+            double quizgrade;
+
+            Console.WriteLine("What is your overall percentage grade for quizzes? ");
+            string quiz = Console.ReadLine();
+            quizgrade = Convert.ToDouble(quiz);
+
+            double midtermgrade;
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            string midterm = Console.ReadLine();
+            midtermgrade = Convert.ToDouble(midterm);
+
+            double fgrade;
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            string final = Console.ReadLine();
+            fgrade = Convert.ToDouble(final);
+
+            double finalsgrade = (homeworkgrade * .08) + (participationgrade * .12) + (midtermgrade * .40) + (fgrade * .40);
+            Console.WriteLine($"{firstname} {lastname} ({studentid}) , your final grade for the course is {finalsgrade.ToString("N0")} %");
+
+        }
+    }
+}
