2d8c214b70f36b1e282a150b194ce4ce2a32630a
Maya Bloom <Maya.Bloom@ou.edu>
2021-04-05 13:05:41 -0500

    completed in class example on methods

---
+++ In class exam 4-5/In class exam 4-5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace In_class_exam_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Please input your grade in the course >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                string lg = ConvertGradetoLetterGrade(grade);
+                Console.WriteLine($"Congratulations you have an {lg} in the course");
+
+                Console.WriteLine("Do you want to convert another grade? Yes or No >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+        }
+
+        /// <summary>
+        /// Convert a standard grade (e.g. 98.5) to the appropraite letter grade (e.g. A)
+        /// </summary>
+        /// <param name="anyVariableNameWeWantThisBelongsToThisMethod"> The grade to convert to a letter grade (e.g. 98)</param>
+        /// <returns> Returns a string that is the letter grade of the input. N/A if it is below 0</returns>
+        static string ConvertGradetoLetterGrade(double anyVariableNameWeWantThisBelongsToThisMethod)
+        {
+            string letterGrade;
+            if (anyVariableNameWeWantThisBelongsToThisMethod >=90)
+            {
+                letterGrade = "A";
+            }
+            else if (anyVariableNameWeWantThisBelongsToThisMethod >=80)
+            {
+                letterGrade = "B";
+            }
+            else if (anyVariableNameWeWantThisBelongsToThisMethod >=70)
+            {
+                letterGrade = "C";
+            }
+
+            else if (anyVariableNameWeWantThisBelongsToThisMethod >=60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "N/A";
+            }
+
+            return letterGrade;
+        }
+    }
+}
---
+++ In class example - methods 3-31/participation methods/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace participation_methods
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            double sum = Add(3, 4); //argument
+            Console.WriteLine(sum);
+
+            double sub = Subtract(7, 4);
+            Console.WriteLine(sub);
+
+            double mul = Multiply(3, 5);
+            Console.WriteLine(mul);
+
+            double div = Divide(10, 5);
+            Console.WriteLine(div);
+
+            int answer;
+
+            Console.WriteLine(" What calculation would you like to perform?");
+            Console.WriteLine(" 1 for Addition");
+            Console.WriteLine(" 2 for Subtraction");
+            Console.WriteLine(" 3 for Multiplication");
+            Console.WriteLine(" 4 for Division");
+            answer = Convert.ToInt32(Console.ReadLine());
+
+            if (answer == 1 )
+            {
+                Console.WriteLine(sum);
+            }
+            else if (answer == 2)
+            {
+                Console.WriteLine(sub);
+            }
+
+            else if (answer == 3)
+            {
+                Console.WriteLine(mul);
+            }
+            else
+            {
+                Console.WriteLine(div);
+            }
+
+            
+
+        }
+
+
+        /// <summary>
+        /// Sums up two values 
+        /// </summary>
+        /// <param name="val1"> The left hand operand of the equation </param>
+        /// <param name="val2"> The right hand operand of the equation</param>
+        /// <returns> the sum of the two numbers </returns>
+        static double Add(double val1, double val2) //parameters are val1 and val2 
+        {
+            double number = val1 + val2;
+            return number;
+            
+
+            //return val1 + val2;
+        }
+
+        static double Subtract(double val1, double val2)
+        {
+            double number = val1 - val2;
+            return number;
+        }
+
+        static double Multiply(double val1, double val2)
+        {
+            double number = val1 * val2;
+            return number;
+
+        }
+
+        static double Divide(double val1, double val2)
+        {
+            double number = val1 / val2;
+            return number;
+        }
+
+       
+
+
+    }
+}
